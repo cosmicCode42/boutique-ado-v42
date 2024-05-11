bdfdb3868b9437a5eedf2d55348f063c5294f01e
cosmicCode42 <cosmicanswer@gmail.com>
2024-05-11 01:07:08 +0000

    properly added docstring to home views.py

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # a view to return the index page
+    """ a view to return the index page """
     return render(request, 'home/index.html')
